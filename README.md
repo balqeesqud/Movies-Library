930d282b335432d742aca4049f18ec03bb0853aa
balqees <balqeesalqudah97@gmail.com>
2023-06-22 00:59:40 +0300

    3rd API added

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## WRRC
 
-![WWRC image](./assets/image1.jpg)
+![WRRC image](./assets/image2.jpg)
 
 ## Overview
 
@@ -13,7 +13,7 @@ setting up a server for a movie app and create the basic structures, that  can c
 ## Getting Started
 
 -**First step**
-Creating a new reposotry, and checkout a new branch.
+Creating a new repository, and checkout a new branch.
 
 -**Second step**
 To start using packages by run command(npm init -y), it will create a default package (package.json) which will hold the description of your file and dependencies that have been installed.
@@ -35,7 +35,11 @@ Creating Handler functions that takes two parameters request and response
 function handleHome(req,res){
   res.send("Welcome to our home page")
 } here the response will be displayed on my server when i run it by using(.send)
+-**Seventh step**
+Adding a 3rd API which is TMDB to get data using axios library along with async/await keywords.
 
 ## Project Features
 
-project holds the port will be running on which is here 3001, and also the get routes are ("/") for home page, ("/favorite") for favorite page, and ("*") for any other resources, and for their handler functions; home handler function will resend a json formatted of the movie we have on our server, and favorite handle function will resend("welcome to our favorite page") and lastly for handleing the errors for typing any other routes will provide error 404, and also added a server error function if that server has some error.
+project holds the port will be running on which is here 3001, and also the get routes are ("/") for home page, ("/favorite") for favorite page, and ("*") for any other resources, and for their handler functions; home handler function will resend a json formatted of the movie we have on our server, and favorite handle function will resend("welcome to our favorite page") and lastly for handling the errors for typing any other routes will provide error 404, and also added a server error function if that server has some error.
+
+**Updates** send get requests to a 3rd party API and display data; project now holds also the ('/search'), ('/trending'), ('/top_rated'), and ('/tv_list') endpoints, to search for any movie, trending, top 10 movies, or for tv lists.
--- server.js
+++ server.js
@@ -3,29 +3,17 @@
 const express = require('express');
 const movieData = require('./Movie Data/data.json'); // importing data from json file and save it in a variable
 const cors = require('cors');
-
 const app = express();
+const axios = require('axios');
 
 app.use(cors()); // who can touch the server
 
 // Error handling middleware
-app.use(handleServerError);
+// app.use(handleServerError);
 
 //Routes
 app.get('/', handleHome);
 app.get('/favorite', handleFavorite);
-app.get('*', handleNotFound);
-
-//constructor function                 ( since the response example look like json map )
-
-function HomeMovie(title, poster_path, overview) {
-  //form to be followed for other movies
-  this.title = title;
-  this.poster_path = poster_path;
-  this.overview = overview;
-}
-
-// {
 
 //Handlers Functions
 function handleHome(req, res) {
@@ -39,29 +27,152 @@ function handleHome(req, res) {
 }
 
 function handleFavorite(req, res) {
-  res.send('Welcome to Favorite Page');
+  res.send('Welcome to our Favorite page');
 }
 
-// Server Error
-function handleServerError(err, req, res, next) {
-  res.status(500).json({
+
+app.get('/trending', async (req, res) => {
+  let axiosRespone = await axios.get(
+    'https://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US'
+  );
+  // res.send(axiosRespone.data);
+
+  // since its an object of two parameters we want to access results then it will be an
+  // array of objects we can access to each object using dot notation/ bracket
+  // and it can be solved using a constructor function or creating map (key:value) pairs , using in both for loop
+  let arrayData = axiosRespone.data['results'];
+  let trendingMovies = [];
+  for (let i = 0; i < arrayData.length; i++) {
+    // let mov = new TrendingMovie(
+    //   arrayData[i]['id'],
+    //   arrayData[i]['title'],
+    //   arrayData[i]['poster_path'],
+    //   arrayData[i]['release_date'],
+    //   arrayData[i]['overview']
+    // );
+    let mov = {
+      //grouping data in group value pairs
+      id: arrayData[i].id,
+      title: arrayData[i].title,
+      release_date: arrayData[i].release_date,
+      poster_path: arrayData[i].poster_path,
+      overview: arrayData[i].overview,
+    };
+    trendingMovies.push(mov);
+  }
+  res.send(trendingMovies);
+});
+
+// function TrendingMovie(id, title, poster_path, release_date, overview) {
+//// form to be followed for other movies
+//   this.id = id;
+//   this.title = title;
+//   this.poster_path = poster_path;
+//   this.release_date = release_date;
+//   this.overview = overview;
+// }
+
+app.get('/search', async (req, res) => {
+  let movieName = req.query.name;
+  let axiosRespone = await axios.get(
+    'https://api.themoviedb.org/3/search/movie?api_key=668baa4bb128a32b82fe0c15b21dd699&language=en-US&query=The&page=2'
+  );
+  let searchArray = axiosRespone.data['results'];
+  for (let i = 0; i < searchArray.length; i++) {
+    if (
+      movieName.toLowerCase().trim() === // trim to remove white-spaces
+      searchArray[i].title.toLowerCase().trim()
+    ) {
+      // console.log(movieName);
+      // console.log(searchArray[i].title);
+
+      let movie = {
+        id: searchArray[i].id,
+        title: searchArray[i].title,
+        poster_path: searchArray[i].poster_path,
+        release_date: searchArray[i].release_date,
+        overview: searchArray[i].overview,
+      };
+      console.log(movie);
+      return res.send(movie);
+    }
+  }
+  res.send('Movie not found');
+});
+
+app.get('/top_rated', async (req, res) => {
+  let axiosRespone = await axios.get(
+    'https://api.themoviedb.org/3/movie/top_rated?api_key=668baa4bb128a32b82fe0c15b21dd699&language=en-US&page=1'
+  );
+  let topRatedArr = axiosRespone.data['results'];
+  let rated = [];
+  let movie;
+  for (let i = 0; i < 10; i++) {
+    movie = {
+      id: topRatedArr[i].id,
+      title: topRatedArr[i].title,
+      vote_average: topRatedArr[i].vote_average,
+      release_date: topRatedArr[i].release_date,
+      overview: topRatedArr[i].overview,
+    };
+    console.log(movie);
+    rated.push(movie); // pushing each movie to the array
+  }
+  res.send(rated);
+});
+
+app.get('/tv_list', async (req, res) => {
+  let axiosRespone = await axios.get(
+    ' https://api.themoviedb.org/3/genre/tv/list?api_key=668baa4bb128a32b82fe0c15b21dd699&language=en-US&page=1'
+  );
+
+  let tvArray = axiosRespone.data['genres'];
+  let tvListArr = [];
+  let category;
+
+  for (let i = 0; i < tvArray.length; i++) {
+    category = {
+      id: tvArray[i].id,
+      name: tvArray[i].name,
+    };
+    tvListArr.push(category);
+  }
+  res.send(tvListArr);
+});
+
+//constructor function                 ( since the response example look like json map )
+function HomeMovie(title, poster_path, overview) {
+  //form to be followed for other movies
+  this.title = title;
+  this.poster_path = poster_path;
+  this.overview = overview;
+}
+
+// {
+
+
+// Server Error(4 parameters)  here is a middleware
+app.use((err, req, res, next) => {
+  res.status(500).send({
+    // we can use .json or .send
     status: 500,
     responseText: 'Sorry, something went wrong',
   });
-}
+});
 
-//Other Routes
-function handleNotFound(req, res) {
-  res.status(404).json({
+// handleNotFound(req, res)  here is a middleware
+app.use((req, res, next) => {
+  res.status(404).send({
+    // we can use .json or .send
     status: 404,
     responseText: 'Page not found',
   });
-}
+});
 
 // Starting the server handler
 
-app.listen(3001, startingLog);
+app.listen(3000, startingLog);
 
 function startingLog(req, res) {
-  console.log('Running at 3001');
+  console.log('Running at 3000');
 }
