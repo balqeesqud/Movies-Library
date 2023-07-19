afaa32d87e8f300ca721ef47dfa6d2bfaef02a28
balqees <balqeesalqudah97@gmail.com>
2023-07-19 11:24:51 +0300

    changed year to varchar

--- movies.schema.sql
+++ movies.schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS movies;
 CREATE TABLE IF NOT EXISTS movies(
   id SERIAL PRIMARY KEY,
   title VARCHAR(200),
-  release_year INTEGER,
+  release_year VARCHAR(200),
   overView VARCHAR(200),
   comment VARCHAR(200)
 ); 
